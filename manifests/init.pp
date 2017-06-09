de56ec1d84bd49b90afd1ca172ad913c7420c2e9
neilmillard-msm <neil.millard@moneysupermarket.com>
2017-06-09 10:31:59 +0100

    createtruststore rename linted

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,7 @@
+#
+#
 class java (
-  $source_url          = "https://download.oracle.com/otn-pub/java/jdk/7u51-b13",
+  $source_url          = 'https://download.oracle.com/otn-pub/java/jdk/7u51-b13',
   $java_major_version  = 7,
   $java_minor_version  = 51,
   $additional_versions = {},
@@ -27,31 +29,31 @@ class java (
     ensure  => file,
     owner   => root,
     group   => root,
-    mode    => 644,
-    content => "export JAVA_HOME=/usr/java/default",
+    mode    => '0644',
+    content => 'export JAVA_HOME=/usr/java/default',
   }
 
   create_resources ( 'install_version', $additional_versions )
   java::install_version {"${java_major_version}_${java_minor_version}": }
 
-  file { "default java":
-    path    => '/usr/java/default',
+  file { 'default java':
     ensure  => 'link',
+    path    => '/usr/java/default',
     mode    => '0755',
     target  => "/usr/java/jdk1.${java_major_version}.0_${java_minor_version}",
-    require => Package["jdk 1.${java_major_version}.0_${java_minor_version}-fcs"]
+    require => Package["jdk 1.${java_major_version}.0_${java_minor_version}-fcs"],
   }
   $fetch_cert = hiera_hash('java::create_truststore', undef)
   if ($fetch_cert) {
     $_hostname = keys($fetch_cert)
-    $_pass = $fetch_cert["$_hostname"][key]
-    $_port = $fetch_cert["$_hostname"][port]
+    $_pass = $fetch_cert["${_hostname}"][key]
+    $_port = $fetch_cert["${_hostname}"][port]
 
-    class { 'java::create-truststore':
-      hostname => $_hostname,
-      port => $_port,
+    class { 'java::createtruststore':
+      hostname   => $_hostname,
+      port       => $_port,
       passphrase => $_pass,
-      require => File["/usr/java/default"]
+      require    => File['default java'],
     }
   }
 
--- manifests/jce.pp
+++ manifests/jce.pp
@@ -1,29 +1,30 @@
+#
+#
 class java::jce (
-    $java_major_version,
-    $jdk_path
-  ) {
-
+  $java_major_version,
+  $jdk_path
+) {
   # puppet_java creates standard java home
   # export JAVA_HOME=/usr/java/default
 
-  file { "$jdk_path/jre/lib/security/README.txt":
-    source => "puppet:///modules/java/jce/$java_major_version/README.txt",
+  file { "${jdk_path}/jre/lib/security/README.txt":
+    source => "puppet:///modules/java/jce/${java_major_version}/README.txt",
     mode   => '0644',
     owner  => 'root',
-    group  => 'root'
+    group  => 'root',
   }
 
-  file { "$jdk_path/jre/lib/security/local_policy.jar":
-    source => "puppet:///modules/java/jce/$java_major_version/local_policy.jar",
+  file { "${jdk_path}/jre/lib/security/local_policy.jar":
+    source => "puppet:///modules/java/jce/${java_major_version}/local_policy.jar",
     mode   => '0644',
     owner  => 'root',
-    group  => 'root'
+    group  => 'root',
   }
 
-  file { "$jdk_path/jre/lib/security/US_export_policy.jar":
-    source => "puppet:///modules/java/jce/$java_major_version/US_export_policy.jar",
+  file { "${jdk_path}/jre/lib/security/US_export_policy.jar":
+    source => "puppet:///modules/java/jce/${java_major_version}/US_export_policy.jar",
     mode   => '0644',
     owner  => 'root',
-    group  => 'root'
+    group  => 'root',
   }
 }
